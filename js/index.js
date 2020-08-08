5593493b2302d3be6830f203b2bb5ba2be2c3185
Elena Lee <jelee.elena@gmail.com>
2020-08-09 00:24:28 +0900

    Make Landing Page that changes background depending on the time

---
+++ js/index.js
@@ -0,0 +1,107 @@
+// DOM Elements
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+
+// Options
+const showAmPm = true;
+
+// Show Time
+
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  // SET AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12 hr format
+  hour = hour % 12 || 12;
+
+  // Output
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${showAmPm ? amPm : ""}`;
+
+  setTimeout(showTime, 1000);
+}
+
+//Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// Change Background and Greeting
+function setBackground() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour > 04 && hour < 12) {
+    document.body.style.backgroundImage = "url('../assets/morning.jpg')";
+    greeting.textContent = "Good Morning";
+  } else if (hour > 12 && hour < 18) {
+    document.body.style.backgroundImage = "url('../assets/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+    document.body.style.color = "black";
+  } else {
+    document.body.style.backgroundImage = "url('../assets/night.jpg')";
+    greeting.textContent = "Good Evening";
+  }
+}
+
+// Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+// Set Name
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
+// Run
+showTime();
+setBackground();
+getName();
+getFocus();
